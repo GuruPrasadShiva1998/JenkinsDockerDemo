e932ca0d2c9a68c5df81a768c0e8fca73006d1a3
guru.prasad <guru.prasad@mediassist.com>
2026-08-18 12:58:37 +0530

    Initial commit

---
+++ JenkinsDockerDemo/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsDockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello from Jenkins + Docker!"
+            });
+        }
+    }
+}
